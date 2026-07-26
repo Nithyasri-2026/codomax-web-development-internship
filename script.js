2bfbf249571597a0d8809744a46c721f8d5b2608
Nithyasri-2026 <nithyasriperiyakaruppan20@gmail.com>
2026-07-26 18:25:00 +0530

    Add null checks for top button in script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Contact Button Scroll
-
 const contactBtn = document.querySelector("#hero button");
 
 if (contactBtn) {
@@ -11,22 +10,23 @@ if (contactBtn) {
 }
 
 // Scroll to Top Button
-
 const topBtn = document.getElementById("topBtn");
 
-// Show button after scrolling 300px
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
-});
+if (topBtn) {
 
-// Scroll to top when clicked
-topBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
     });
-});
+
+    topBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+}
